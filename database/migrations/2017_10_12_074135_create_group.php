04a37ba7d93d6faac2a1063a8f2cc9f18579f014
Hooff,Sander S.M.J. van <s.vanhooff@student.fontys.nl>
2017-10-12 09:55:50 +0200

    added migrations: - create_group - create_peleton

---
+++ database/migrations/2017_10_12_074135_create_group.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroup extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('group');
+    }
+}
