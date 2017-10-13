66192f57d299480df996592a0683a9535cd7f66a
Hooff,Sander S.M.J. van <s.vanhooff@student.fontys.nl>
2017-10-13 12:00:01 +0200

    Create layout page, login template and include pages (jumbotron + navbar)

---
+++ resources/views/includes/jumbotron.blade.php
@@ -0,0 +1,3 @@
+<div class="jumbotron">
+    <h1 class="text-center">Page</h1>
+</div>
---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav id="main-nav" class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+                <img alt="Planner" src="...">
+            </a>
+        </div>
+    </div>
+</nav>
