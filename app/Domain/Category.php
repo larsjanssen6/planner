a3040a98323d86bc1d701d1eaa677f3fdb62767c
Lars Janssen <larsjanssen64@gmail.com>
2017-10-23 20:33:16 +0200

    Role & permission support

--- app/Domain/Category.php
+++ app/Domain/Category.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App;
+namespace App\Domain;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     protected $table = 'category';
+
+    protected $fillable  = ['name', 'type'];
 }
