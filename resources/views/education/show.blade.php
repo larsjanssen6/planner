237610fc5cff64c71223829a2f5082d7c48f0890
Lars Janssen <larsjanssen64@gmail.com>
2017-11-02 11:36:58 +0100

    Show vehicles & add spacing

--- resources/views/education/show.blade.php
+++ resources/views/education/show.blade.php
@@ -26,18 +26,55 @@
                 <div class="is-clearfix"></div>
                 <hr>
 
-            <h2 class="title is-2">{{ $education->name }}</h2>
-            <p><strong>Categorie:</strong> {{ $education->category->name }}</p>
-            <p><strong>Duur in dagen:</strong> {{ $education->duration }}</p>
-            <p><strong>Aantal studenten:</strong> {{ $education->required_students }}</p>
-            <p><strong>Aantal instructeurs:</strong> {{ $education->required_instructors }}</p>
-            <p><strong>Aangemaakt:</strong> {{ $education->created_at }}</p>
-            <p><strong>Laasts bijgewerkt:</strong> {{ $education->updated_at }}</p>
-            <hr>
-            <a href="{{ route('education.edit', ['id' => $education->id]) }}" class="button is-primary is-outlined">Bewerk opleiding</a>
-            @else
-                <div class="is-clearfix"></div>
+                <h2 class="title is-2">{{ $education->name }}</h2>
+
+                <p>
+                    <strong>Categorie:</strong>
+                    {{ $education->category->name }}
+                </p>
+
+                <p>
+                    <strong>Voertuigen:</strong> <br>
+
+                    @foreach($education->vehicles as $vehicle)
+                        <span class="tag is-success">
+                            {{ $vehicle->name }}
+                        </span>
+                    @endforeach
+                </p>
+
+                <p>
+                    <strong>Duur in dagen:</strong>
+                    {{ $education->duration }}
+                </p>
+
+                <p>
+                    <strong>Aantal studenten:</strong>
+                    {{ $education->required_students }}
+                </p>
+
+                <p>
+                    <strong>Aantal instructeurs:</strong>
+                    {{ $education->required_instructors }}
+                </p>
+
+                <p>
+                    <strong>Aangemaakt:</strong>
+                    {{ $education->created_at->diffForHumans() }}
+                </p>
+
+                <p>
+                    <strong>Laasts bijgewerkt:</strong>
+                    {{ $education->updated_at->diffForHumans() }}
+                </p>
+
                 <hr>
+
+                <a href="{{ route('education.edit', ['id' => $education->id]) }}" class="button is-primary is-outlined">
+                    Bewerk opleiding
+                </a>
+            @else
+                <div class="is-clearfix"></div> <hr>
                 <p>De opleiding kon niet worden geladen.</p>
             @endif
         </div>
