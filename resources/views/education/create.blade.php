a57a9fbc07334fdace6dfb92a9ad1f427db13756
Lars Janssen <larsjanssen64@gmail.com>
2017-11-02 11:37:25 +0100

    Add multiselect

--- resources/views/education/create.blade.php
+++ resources/views/education/create.blade.php
@@ -41,9 +41,11 @@
                 </div>
                 <div class="field">
                     {{Form::label('vehicle_id', 'Voertuig', ['class' => 'label'])}}
-                    <div class="select">
-                        {{Form::select('vehicle_id', $vehicles, null, ['class' => 'input', 'placeholder' => 'Kies een voertuig...', 'required' => 'required'])}}
-                    </div>
+
+                    <multi-select prp-name="vehicles"
+                                  :prp-options="{{ json_encode($vehicles) }}"
+                                  prp-placeholder="Kies voertuig(en)">
+                    </multi-select>
                 </div>
                 <div class="field">
                     {{Form::label('duration', 'Duur in dagen', ['class' => 'label'])}}
@@ -59,57 +61,6 @@
                 </div>
                 {{ Form::submit('Maak opleiding aan', ['class' => 'button is-primary is-outlined']) }}
             {!! Form::close() !!}
-
-
-            {{--<form method="POST">--}}
-                {{--{{ csrf_field() }}--}}
-
-                {{--<input name="_method" type="hidden" value="PUT">--}}
-
-                {{--@component('layouts/input', [--}}
-                                    {{--'name' => 'name',--}}
-                                    {{--'label' => 'Naam'--}}
-                                {{--])--}}
-                {{--@endcomponent--}}
-
-                {{--TODO: dropdown fixen--}}
-                {{--@component('layouts/dropdown', [--}}
-                                   {{--'name' => 'category',--}}
-                                   {{--'label' => 'Categorie',--}}
-                                   {{--'array' => $categories,--}}
-                                   {{--'value' => 'id',--}}
-                                   {{--'option' => 'name',--}}
-                                   {{--'selected' => 1--}}
-                               {{--])--}}
-                {{--@endcomponent--}}
-
-                {{--@component('layouts/input', [--}}
-                                    {{--'name' => 'duration',--}}
-                                    {{--'label' => 'Duur in dagen',--}}
-                                    {{--'type' => 'number'--}}
-                                {{--])--}}
-                {{--@endcomponent--}}
-
-                {{--@component('layouts/input', [--}}
-                                    {{--'name' => 'required_students',--}}
-                                    {{--'label' => 'Aantal studenten',--}}
-                                    {{--'type' => 'number'--}}
-                                {{--])--}}
-                {{--@endcomponent--}}
-
-                {{--@component('layouts/input', [--}}
-                                    {{--'name' => 'required_instructors',--}}
-                                    {{--'label' => 'Aantal instructeurs',--}}
-                                    {{--'type' => 'number'--}}
-                                {{--])--}}
-                {{--@endcomponent--}}
-
-                {{--<div class="control">--}}
-                    {{--<button type="submit" class="button is-primary is-outlined">--}}
-                        {{--Maak opleiding aan--}}
-                    {{--</button>--}}
-                {{--</div>--}}
-            {{--</form>--}}
         </div>
     </div>
 @endsection
